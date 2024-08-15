fc1a9dc4a36fb942e6cc95034975e74f138c6e32
Brice KEBERE <bkebere10@gmail.com>
2024-08-15 23:28:21 +0000

    Built POST, GET, DELETE, PUT functions

--- src/app/api/topics/route.js
+++ src/app/api/topics/route.js
@@ -9,10 +9,24 @@ export async function POST(request) {
     await connectMongoDB();
     await Topic.create({title,description})
     return NextResponse.json({
-        message:'Topic creayed'
+        message:'Topic created'
     },
 {
     status:201
 })
+}
 
+export async function GET(){
+    await connectMongoDB();
+    const topics = await Topic.find();
+    return NextResponse.json({topics})
+}
+
+export async function DELETE (request){
+    const id = request.nextUrl.searchParams.get('id');
+    await connectMongoDB();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json(
+        {message:'Topic deleted'},{status:200}
+    )
 }
